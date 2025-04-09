c635fe623f02b440c2a7bd4c695846221db3e6df
Asilbek <asilbek.sadullayev000@example.com>
2025-04-09 18:27:56 +0500

    bazi bir

--- apps/users/utils.py
+++ apps/users/utils.py
@@ -9,8 +9,8 @@ def generate_confirmation_code():
 
 
 def send_confirmation_code_to_user(user, code):
-    subject = 'Soffwork.uz | Tasdiqlash Kodingiz'
-    message = f'Sizning soffwork.uz uchun tasdiqlash kodingiz: {code}'
+    subject = 'Tasdiqlash Kodingiz'
+    message = f'Sizning tasdiqlash kodingiz: {code}'
     from_email = settings.EMAIL_HOST_USER
     recipient_list = [user.email]
     try:
@@ -21,24 +21,24 @@ def send_confirmation_code_to_user(user, code):
 
 def send_verification_code_to_user(phone, code):
     print(f"Verification code: {str(code)}")
-    # message_id = str(timezone.now())  # noqa
-    # requests.post(
-    #     settings.SMS_URL,
-    #     auth=(settings.SMS_LOGIN, settings.SMS_PASSWORD),
-    #     json={
-    #         "messages": [
-    #             {
-    #                 "recipient": str(phone),
-    #                 "message-id": message_id,
-    #                 "sms": {
-    #                     "originator": "3700",
-    #                     "content": {
-    #                         "text": f"soffwork.uz <#> Sizning tasdiqlash kodingiz {code}"
-    #                     },
-    #                 },
-    #             }
-    #         ]
-    #     },
-    # )
+    message_id = str(timezone.now())  # noqa
+    requests.post(
+        settings.SMS_URL,
+        auth=(settings.SMS_LOGIN, settings.SMS_PASSWORD),
+        json={
+            "messages": [
+                {
+                    "recipient": str(phone),
+                    "message-id": message_id,
+                    "sms": {
+                        "originator": "3700",
+                        "content": {
+                            "text": f" <#> Sizning tasdiqlash kodingiz {code}"
+                        },
+                    },
+                }
+            ]
+        },
+    )
 
 
